1068a9af11bfa31aa4d7c3ec0c9804383025377c
jion-kozono <zeon66190329@gmail.com>
2019-11-17 14:36:24 +0900

    first commit 2019_11_17

---
+++ blogapi/posts/views.py
@@ -0,0 +1,14 @@
+from django.contrib.auth import get_user_model
+from rest_framework import viewsets
+from .models import Post
+from .permissions import IsAuthorOrReadOnly
+from .serialisers import PostSerializer, UserSerializer
+
+class PostViewSet(viewsets.ModelViewSet):
+    permission_classes= (IsAuthorOrReadOnly,)
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
